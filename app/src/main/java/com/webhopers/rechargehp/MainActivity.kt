fc6d5e93e9c039242b9e392935f6049855a6c59a
AnujWebhopers <anuj.webhopers@gmail.com>
2022-03-14 09:54:17 +0530

    App already live in Client personal Google play store account. complete code push.

---
+++ app/src/main/java/com/webhopers/rechargehp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.webhopers.rechargehp
+
+import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webView = findViewById(R.id.webview)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.setAppCacheEnabled(true);
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                url?.let { view?.loadUrl(it) }
+                return true
+            }
+        }
+        webView.loadUrl("https://rechargehp.com/dev/login")
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+}
